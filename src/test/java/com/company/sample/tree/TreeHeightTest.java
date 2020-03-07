9b523794c988505ad8dad2a304dc928d78f0553a
rikim <rikim@riotgames.com>
2020-03-07 23:19:18 +0900

    Add test cases TreeHeightTest

--- src/test/java/com/company/sample/tree/TreeHeightTest.java
+++ src/test/java/com/company/sample/tree/TreeHeightTest.java
@@ -4,56 +4,76 @@ package com.company.sample.tree;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-class GetTreeHeightTest {
-    private static final GetTreeHeight getTreeHeight = new GetTreeHeight();
+class TreeHeightTest {
+    private static TreeHeight treeHeight = new TreeHeight();
 
     @Test
-    public void testGetTreeHeight_whenTwoNodesToFindAreParentAndChild() {
+    public void getDistanceBetween_whenOneNodeIsRootAndAnotherNodeIsItsChild_thenDistanceShouldBe1() {
         // Given
         Node root = new Node("A");
-        String childToFind = "C";
-        root.addChildren("B", childToFind);
+        String pointA = "A";
+        String pointC = "C";
+        root.addChildren("B", pointC);
 
         // When
-        int height = getTreeHeight.getHeightFromChild(root, childToFind);
+        int height = treeHeight.getDistanceBetween(root, pointA, pointC);
 
         // Then
         Assertions.assertThat(height).isEqualTo(1);
     }
 
     @Test
-    public void testGetTreeHeight_whenTwoNodesToFindAreParentAndChild_case2() {
+    public void getDistanceBetween_whenOneIsRootAndAnotherIsItsGrandChild_thenDistanceShouldBe2() {
         // Given
         Node root = new Node("A");
-        String toFind = "E";
+        String pointA = "A";
+        String pointE = "E";
         root.addChildren("B", "C");
-        Node rootChild = new Node("D");
-        rootChild.addChildren(toFind);
-        root.addChild(rootChild);
+        Node childOfRoot = new Node("D");
+        Node grandChildOfRoot = new Node(pointE);
+        childOfRoot.addChild(grandChildOfRoot);
+        root.addChild(childOfRoot);
 
 
         // When
-        int height = getTreeHeight.getHeightFromChild(root, toFind);
+        int height = treeHeight.getDistanceBetween(root, pointA, pointE);
 
         // Then
         Assertions.assertThat(height).isEqualTo(2);
     }
 
     @Test
+    public void getDistanceBetween_whenOneIsChildOfRootAndAnotherIsGrandChild_thenDistanceShouldBe3() {
+        // Given
+        Node root = new Node("A");
+        String pointB = "B";
+        String pointE = "E";
+        root.addChildren("B", "C");
+        Node rootChild = new Node("D");
+        rootChild.addChildren(pointB);
+        root.addChild(rootChild);
+
+
+        // When
+        int height = treeHeight.getDistanceBetween(root, pointB, pointE);
+
+        // Then
+        Assertions.assertThat(height).isEqualTo(3);
+    }
+
+    @Test
     public void testGetTreeHeight_whenNoChildToFindExists() {
         // Given
         Node root = new Node("A");
-        String childToFind = "D";
+        String pointA = "A";
+        String notExistingPoint = "D";
         root.addChildren("B", "C");
 
         // When
+        int distanceBetween = treeHeight.getDistanceBetween(root, pointA, notExistingPoint);
 
-        int height = 0;
-        for (Node child : root.children) {
-            height += getTreeHeight.getHeightFromChild(root, childToFind);
-        }
 
         // Then
-        Assertions.assertThat(height).isEqualTo(0);
+        Assertions.assertThat(distanceBetween).isEqualTo(0);
     }
 }
