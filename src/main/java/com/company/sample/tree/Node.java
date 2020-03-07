b345c4733e20dc70fe840e687ebe006ecce23521
rikim <rikim@riotgames.com>
2020-03-07 23:16:35 +0900

    Add Node entity

--- src/main/java/com/company/sample/tree/Node.java
+++ src/main/java/com/company/sample/tree/Node.java
@@ -1,4 +1,23 @@
 package com.company.sample.tree;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Node {
+    public Node(String value) {
+        this.value = value;
+    }
+
+    public List<Node> children = new LinkedList<>();
+    public String value;
+
+    public void addChild(Node child) {
+        children.add(child);
+    }
+
+    public void addChildren(String ...values) {
+        for (String value : values) {
+            addChild(new Node(value));
+        }
+    }
 }
