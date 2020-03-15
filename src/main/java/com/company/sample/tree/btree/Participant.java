a34e0699ac00b91ea9d5089b6cd5357db74c373c
rikim <rikim@riotgames.com>
2020-03-15 23:39:48 +0900

    Add default contructor

--- src/main/java/com/company/sample/tree/btree/Participant.java
+++ src/main/java/com/company/sample/tree/btree/Participant.java
@@ -3,4 +3,9 @@ package com.company.sample.tree.btree;
 public class Participant {
     public int solvedProblems;
     public int eatenRamens;
+
+    public Participant(int solvedProblems, int eatenRamens) {
+        this.solvedProblems = solvedProblems;
+        this.eatenRamens = eatenRamens;
+    }
 }
