0c28b728b00ca8f4295ee24dcd0720a2aeb83c96
rikim <rikim@riotgames.com>
2020-05-03 16:31:29 +0900

    Add test case of pushing non-min value

--- src/test/java/com/company/stack/MinStackImplementationTest.java
+++ src/test/java/com/company/stack/MinStackImplementationTest.java
@@ -22,4 +22,21 @@ public class MinStackImplementationTest {
         Assertions.assertThat(min).isEqualTo(theOnlyOneNumber);
     }
 
+    @Test
+    public void getMin_whenLastInValueIsNotTheMinValue_andWhenTheLastInValueIsPushed_thenTheMinValuesIsEqualToTheValueBeforePushing() {
+        // Given
+        MinStack minStackImplementation = new MinStackImplementation();
+        int firstInValue = 1;
+        minStackImplementation.push(firstInValue);
+        int minValueBeforePush = minStackImplementation.getMin();
+
+        // When
+        int LastInValue = 10;
+        minStackImplementation.push(firstInValue);
+        int min = minStackImplementation.getMin();
+
+        // Then
+        Assertions.assertThat(min).isEqualTo(minValueBeforePush);
+    }
+
 }
