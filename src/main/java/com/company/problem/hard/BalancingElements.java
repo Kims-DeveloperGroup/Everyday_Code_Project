78248e0ac31b894e776f4ce7a57d7805d34ec804
rikim <rikim@riotgames.com>
2020-05-10 14:43:26 +0900

    Add break condition not to traverse all

--- src/main/java/com/company/problem/hard/BalancingElements.java
+++ src/main/java/com/company/problem/hard/BalancingElements.java
@@ -2,16 +2,21 @@ package com.company.problem.hard;
 
 public class BalancingElements {
     public int countBalancingElemetns(int[] unbalancedElements) {
+        int wholeSum = 0;
+        for (int number : unbalancedElements) {
+            wholeSum += number;
+        }
+
         int count = 0;
         for (int i = 0; i < unbalancedElements.length; i++) {
-            if (isBalanced(unbalancedElements, i)) {
+            if (isBalanced(unbalancedElements, i, wholeSum)) {
                 count++;
             }
         }
         return count;
     }
 
-    private boolean isBalanced(int[] elements, int balancingElementIndex) {
+    private boolean isBalanced(int[] elements, int balancingElementIndex, int wholeSum) {
         int oddSum = 0;
         int evenSum = 0;
 
@@ -29,6 +34,10 @@ public class BalancingElements {
                 } else {
                     oddSum += elements[i];
                 }
+            }
+
+            if (oddSum > (wholeSum / 2) || (evenSum > (wholeSum / 2))) {
+                break;
             }
         }
         return evenSum == oddSum;
