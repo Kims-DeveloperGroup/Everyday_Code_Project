29b9c77da4d5c32522190f8a970ffd56771f6ec2
rikim <rikim@riotgames.com>
2020-05-10 14:48:18 +0900

    Remove duplicate code

--- src/main/java/com/company/problem/hard/BalancingElements.java
+++ src/main/java/com/company/problem/hard/BalancingElements.java
@@ -21,19 +21,16 @@ public class BalancingElements {
         int evenSum = 0;
 
         for (int i = 0; i < elements.length; i++) {
-            if (i < balancingElementIndex) {
-                if (((i % 2) == 0)) {
-                    evenSum += elements[i];
-                } else {
-                    oddSum += elements[i];
-                }
-            } else if (i > balancingElementIndex) {
-                int repositioned = i - 1;
-                if (((repositioned % 2) == 0)) {
-                    evenSum += elements[i];
-                } else {
-                    oddSum += elements[i];
-                }
+            if (balancingElementIndex == i) {
+                continue;
+            }
+
+            int repositioned = (i > balancingElementIndex) ? i - 1 : i;
+
+            if ((repositioned % 2) == 0) {
+                evenSum += elements[i];
+            } else {
+                oddSum += elements[i];
             }
 
             if (oddSum > (wholeSum / 2) || (evenSum > (wholeSum / 2))) {
