f51c5566641bb475aa0139d141fc323a3d6e265c
rikim <rikim@riotgames.com>
2020-05-16 23:45:23 +0900

    Apply KMP algorithm and build cache with O(N^2)

--- src/main/java/com/company/text/TextSearch.java
+++ src/main/java/com/company/text/TextSearch.java
@@ -1,5 +1,8 @@
 package com.company.text;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class TextSearch {
     private final String text;
 
@@ -8,6 +11,7 @@ public class TextSearch {
     }
 
     public int searchText(String textToSearch) {
+        Map<Integer, Integer> incrementByMatchedPostfix = buildCache(textToSearch);
         int count = 0;
         for (int startIndex = 0; startIndex < text.length() - textToSearch.length() + 1; ) {
             int matchedCharCount = 0;
@@ -23,8 +27,31 @@ public class TextSearch {
             if (matchedCharCount == textToSearch.length()) {
                 count++;
             }
-            startIndex++;
+            startIndex = startIndex + incrementByMatchedPostfix.get(matchedCharCount);
         }
         return count;
+    }
+
+    public Map<Integer, Integer> buildCache(String text) {
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        map.put(text.length(), 1);
+        for (int len = 1; len < text.length(); len++) {
+            int matched = 0;
+            for (int i = 0; i < len; i++) {
+                char pre = text.charAt(i);
+                char post = text.charAt(text.length() - 1 - i);
+
+                if (post == pre) {
+                    matched++;
+                }
+            }
+            if (len == matched) {
+                map.put(len, text.length() - len);
+            } else {
+                map.put(len, 1);
+            }
+        }
+        return map;
     }
 }
