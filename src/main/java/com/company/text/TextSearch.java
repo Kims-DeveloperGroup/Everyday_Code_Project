cf7e9815187cbdea6a26519f089cda4957d6cf68
rikim <rikim@riotgames.com>
2020-05-16 20:43:55 +0900

    Add empty TextSearch and Text case to verify

---
+++ src/main/java/com/company/text/TextSearch.java
@@ -0,0 +1,13 @@
+package com.company.text;
+
+public class TextSearch {
+    private final String text;
+
+    public TextSearch(String text) {
+        this.text = text;
+    }
+
+    public int searchText(String textToSearch) {
+        return 0;
+    }
+}
---
+++ src/test/java/com/company/text/TextSearchTest.java
@@ -0,0 +1,20 @@
+package com.company.text;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class TextSearchTest {
+    @Test
+    public void searchText_whenSearchingAVAInAVAVA_thenReturning2() {
+        // Given
+        String textToSearch = "ava";
+        TextSearch avava = new TextSearch("avava");
+
+        // When
+        int count = avava.searchText(textToSearch);
+
+        // Then
+        assertThat(count).isEqualTo(2);
+    }
+}
