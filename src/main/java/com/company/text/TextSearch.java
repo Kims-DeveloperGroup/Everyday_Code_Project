fced4c1f4bf9cc46eaf13d924711bb8983a50e14
rikim <rikim@riotgames.com>
2020-05-17 00:10:01 +0900

    Revert "Apply KMP algorithm and build cache with O(N^2)"
    This reverts commit f51c5566

--- src/main/java/com/company/text/TextSearch.java
+++ src/main/java/com/company/text/TextSearch.java
@@ -1,8 +1,5 @@
 package com.company.text;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class TextSearch {
     private final String text;
 
@@ -11,7 +8,6 @@ public class TextSearch {
     }
 
     public int searchText(String textToSearch) {
-        Map<Integer, Integer> incrementByMatchedPostfix = buildCache(textToSearch);
         int count = 0;
         for (int startIndex = 0; startIndex < text.length() - textToSearch.length() + 1; ) {
             int matchedCharCount = 0;
@@ -27,31 +23,8 @@ public class TextSearch {
             if (matchedCharCount == textToSearch.length()) {
                 count++;
             }
-            startIndex = startIndex + incrementByMatchedPostfix.get(matchedCharCount);
+            startIndex++;
         }
         return count;
-    }
-
-    public Map<Integer, Integer> buildCache(String text) {
-        Map<Integer, Integer> map = new HashMap<>();
-        map.put(0, 1);
-        map.put(text.length(), 1);
-        for (int len = 1; len < text.length(); len++) {
-            int matched = 0;
-            for (int i = 0; i < len; i++) {
-                char pre = text.charAt(i);
-                char post = text.charAt(text.length() - 1 - i);
-
-                if (post == pre) {
-                    matched++;
-                }
-            }
-            if (len == matched) {
-                map.put(len, text.length() - len);
-            } else {
-                map.put(len, 1);
-            }
-        }
-        return map;
     }
 }
