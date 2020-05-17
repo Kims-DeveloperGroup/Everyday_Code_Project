1fc2b29e6bf964c2467e85cfc1d66c34a3cda30e
rikim <rikim@riotgames.com>
2020-05-17 14:29:34 +0900

    Apply incrementMap when incrementing startIndex in searchText

--- src/main/java/com/company/text/TextSearch.java
+++ src/main/java/com/company/text/TextSearch.java
@@ -1,16 +1,23 @@
 package com.company.text;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class TextSearch {
-    private final String text;
+    public final String text;
+    public Map<Integer, Integer> integerIntegerMap;
 
     public TextSearch(String text) {
         this.text = text;
     }
 
     public int searchText(String textToSearch) {
+        integerIntegerMap = this.buildIncrementMap(textToSearch);
+        System.out.println("start to search");
         int count = 0;
         for (int startIndex = 0; startIndex < text.length() - textToSearch.length() + 1; ) {
             int matchedCharCount = 0;
+            System.out.println("startIndex: " + startIndex);
             for (int currIndex = 0; currIndex < textToSearch.length(); ) {
                 char ch1 = text.charAt(currIndex + startIndex);
                 char ch2 = textToSearch.charAt(currIndex);
@@ -22,9 +29,42 @@ public class TextSearch {
             }
             if (matchedCharCount == textToSearch.length()) {
                 count++;
+                startIndex++;
+            } else if (matchedCharCount == 0 || matchedCharCount == 1) {
+                startIndex++;
+            } else {
+                startIndex = startIndex + this.integerIntegerMap.get(matchedCharCount);
             }
-            startIndex++;
         }
         return count;
+    }
+
+    public Map<Integer, Integer> buildIncrementMap(String text) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int partialLength = 2; partialLength < text.length(); partialLength++) {
+            String partialString = text.substring(0, partialLength);
+            int maxLengthToMatch = getMaxLengthMatchingPrefixAndPostfix(partialString);
+            if (maxLengthToMatch == 0) {
+                map.put(partialLength, 1);
+            } else {
+                map.put(partialLength, partialLength - maxLengthToMatch);
+            }
+        }
+        return map;
+    }
+
+    private int getMaxLengthMatchingPrefixAndPostfix(String subStr) {
+        System.out.println("subString: " + subStr);
+        int maxLength = 0;
+        for (int prefixLength = 1; prefixLength < subStr.length(); prefixLength++) {
+            String pre = subStr.substring(0, prefixLength);
+            String post = subStr.substring(subStr.length() - prefixLength);
+            System.out.println("pre: " + pre + " post: " + post + " prefixLen: " + prefixLength);
+            if (pre.equals(post)) {
+                System.out.println("matched");
+                maxLength = prefixLength;
+            }
+        }
+        return maxLength;
     }
 }
--- src/test/java/com/company/text/TextSearchTest.java
+++ src/test/java/com/company/text/TextSearchTest.java
@@ -2,6 +2,8 @@ package com.company.text;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Map;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class TextSearchTest {
@@ -16,5 +18,48 @@ public class TextSearchTest {
 
         // Then
         assertThat(count).isEqualTo(2);
+    }
+
+    @Test
+    public void buildIncrementMap_whenAAIsGiven_thenIncrementHasNoElement() {
+        // Given
+        String textToSearch = "aa";
+        TextSearch avava = new TextSearch("avava");
+
+        // When
+        Map<Integer, Integer> aaIncrementMap = avava.buildIncrementMap(textToSearch);
+
+        // Then
+        assertThat(aaIncrementMap).hasSize(0);
+    }
+
+    @Test
+    public void buildIncrementMap_when_ABcAB_IsGiven() {
+        // Given
+        String textToSearch = "abcabd";
+        TextSearch avava = new TextSearch("avava");
+
+        // When
+        Map<Integer, Integer> aaIncrementMap = avava.buildIncrementMap(textToSearch);
+
+        // Then
+        assertThat(aaIncrementMap).hasSize(4);
+        assertThat(aaIncrementMap.get(2)).isEqualTo(1);
+        assertThat(aaIncrementMap.get(3)).isEqualTo(1);
+        assertThat(aaIncrementMap.get(4)).isEqualTo(3);
+        assertThat(aaIncrementMap.get(5)).isEqualTo(3);
+    }
+
+    @Test
+    public void searchText_when_ABcAB_IsGiven() {
+        // Given
+        String textToSearch = "abcabd";
+        TextSearch avava = new TextSearch("XXXX" + textToSearch + "XXXX" + textToSearch);
+
+        // When
+        int matchedCount = avava.searchText(textToSearch);
+
+        // Then
+        assertThat(matchedCount).isEqualTo(2);
     }
 }
